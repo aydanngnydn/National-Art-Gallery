17cf9e89844fa5b207475597322e02e0b9d7e9e0
bilgecelikk <jbfmf10@gmail.com>
2023-11-05 18:09:50 +0300

    render_template and homepage route added

--- server.py
+++ server.py
@@ -1,8 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
-def create_app():
-    app = Flask(__name__)
-    return app
+app = Flask(__name__)
+
+@app.route("/")
+def homepage():
+    return render_template("home.html")
 
 if __name__ == "__main__":
     app = create_app()
